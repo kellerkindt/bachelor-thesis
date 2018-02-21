3f9678487431df5066f490da56a380a8653ddc37
Michael Watzko <michael@watzko.de>
2018-02-21 18:17:52 +0100

    Textual refinements

--- doc/sections/rust.position_offset.c
+++ doc/sections/rust.position_offset.c
@@ -1,8 +1,8 @@
 typedef struct PositionOffset {
     long position_north;
     long position_east;
-    long *std_dev_position_north /* OPTIONAL */;
-    long *std_dev_position_east  /* OPTIONAL */;
+    long *std_dev_position_north; // OPTIONAL
+    long *std_dev_position_east;  // OPTIONAL
 
     // ...
 } PositionOffset_t;

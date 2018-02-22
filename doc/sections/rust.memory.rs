b7caf280e478a51fde7a87bf8b598901d0219389
Michael Watzko <michael@watzko.de>
2018-02-22 14:11:46 +0100

    Add rust:scope

---
+++ doc/sections/rust.memory.rs
@@ -0,0 +1,8 @@
+fn main() { // neuer Scope
+    let mut a = Box::new(5); // 5 kommt auf den Heap
+    { // neuer Scope
+        let b = Box::new(10); // 10 kommt auch auf den Heap
+        *a += *b; // a ist nun 15 
+    } // Lebenszeit von b zuende, Speicher wird freigeben
+    println!("a: {}", a); // Ausgabe: "a: 15"
+} // Lebenszeit von a zuende, Speicher wird freigegeben

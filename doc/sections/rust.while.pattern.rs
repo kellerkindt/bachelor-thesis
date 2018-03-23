d462df1740ffbf79093c736acde568c6fa9bc89e
Michael Watzko <michael@watzko.de>
2018-03-23 16:51:37 +0100

    Resolve various todos, mostly about wording

--- doc/sections/rust.while.pattern.rs
+++ doc/sections/rust.while.pattern.rs
@@ -3,7 +3,7 @@ use std::io::stdin;
 fn main() {
     let mut eingabe = String::new();
     while let Ok(_) = stdin().read_line(&mut eingabe) {
-        println!("Eingegeben: {}", eingabe.trim());
+        println!("Eingabe: {}", eingabe.trim());
         eingabe.clear();
     }
 }

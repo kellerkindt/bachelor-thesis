36240f140a53a2893feaf9d8cf0f953fb07d4b31
Michael Watzko <michael@watzko.de>
2018-04-04 15:43:05 +0200

    Add decode and encode for UpdateSubscription

--- source/libmessages/src/lib.rs
+++ source/libmessages/src/lib.rs
@@ -11,7 +11,9 @@ extern crate log4rs;
 #[derive(Debug)]
 pub enum Message {
     Registration(Box<raw::ClientRegistration>),
+    UpdateSubscription(Box<raw::UpdateSubscription>),
 }
+
 
 impl Message {
     pub fn decode_client_registration(buffer: &[u8]) -> Result<Message, ()> {
@@ -20,10 +22,19 @@ impl Message {
         }))
     }
 
+    pub fn decode_update_subscription(buffer: &[u8]) -> Result<Message, ()> {
+        Ok(Message::UpdateSubscription(unsafe {
+            asn::uper_decode(&mut raw::asn_DEF_UpdateSubscription, buffer)?
+        }))
+    }
+
     pub fn encode(&self, target: &mut [u8]) -> Result<usize, ()> {
         match self {
             Message::Registration(ref registration) => unsafe {
                 asn::uper_encode(&mut raw::asn_DEF_ClientRegistration, registration.as_ref(), target)
+            },
+            Message::UpdateSubscription(ref subscription) => unsafe {
+                asn::uper_encode(&mut raw::asn_DEF_UpdateSubscription, subscription.as_ref(), target)
             }
         }
     }
@@ -34,6 +45,7 @@ impl Drop for Message {
         unsafe {
             match self {
                 Message::Registration(ref registration) => asn::free_content(&mut raw::asn_DEF_ClientRegistration, registration.as_ref()),
+                Message::UpdateSubscription(ref subscription) => asn::free_content(&mut raw::asn_DEF_UpdateSubscription, subscription.as_ref()),
             };
         }
     }
@@ -46,7 +58,6 @@ impl Drop for Message {
 mod tests {
     use std::ptr;
     use std::mem;
-    use std::os::raw;
     use raw::*;
     use super::*;
 
@@ -72,6 +83,47 @@ mod tests {
         let _ = log4rs::init_config(config);
     }
 
+    fn test_encode(message: Message, should_be: &[u8]) {
+        let mut buffer = vec![0u8; should_be.len()];
+        let result = message.encode(&mut buffer[..]);
+        trace!("result {:?}", result);
+        if let Ok(count) = result {
+            let mut string = String::new();
+            for byte in buffer[..count].iter() {
+                string.push_str(&format!("{:02x} ", byte));
+            }
+            trace!("{}", string);
+        }
+        assert_eq!(Ok(should_be.len()), result);
+        assert_eq!(should_be, &buffer[..]);
+    }
+
+    #[test]
+    fn encode_update_subscription() {
+        init_logger();
+        let subscription = Message::UpdateSubscription(unsafe {
+            let mut subscription = Box::new(mem::zeroed::<UpdateSubscription>());
+            subscription.subscription_status = SubscriptionStatus::SubscriptionStatus_subscribed as SubscriptionStatus_t;
+            subscription.message_period = ptr::null_mut();
+            subscription
+        });
+        test_encode(subscription, &[0x40, 0x00]);
+    }
+
+    #[test]
+    fn decode_update_subscription() {
+        init_logger();
+        let message = Message::decode_update_subscription(&[0x40, 0x00]);
+        trace!("result: {:?}", message);
+        match message.expect("Message decoding failed") {
+            Message::UpdateSubscription(ref sub) => {
+                assert_eq!(SubscriptionStatus::SubscriptionStatus_subscribed as SubscriptionStatus_t, sub.subscription_status);
+                assert_eq!(ptr::null_mut(), sub.message_period);
+            },
+            _ => panic!("Wrong message variant")
+        }
+    }
+
     #[test]
     fn encode_client_registration() {
         init_logger();
@@ -82,28 +134,22 @@ mod tests {
             registration.minimum_message_period = ptr::null_mut();
             registration
         });
-        let mut buffer = [0u8; 16];
-        assert_eq!(Ok(3), registration.encode(&mut buffer[..]));
-        assert_eq!(&[0x20_u8, 0x00, 0x00], &buffer[..3]);
-        let mut string = String::new();
-        for byte in buffer[..3].iter() {
-            string.push_str(&format!("{:02x} ", byte));
-        }
-        trace!("{}", string);
+        test_encode(registration, &[0x20, 0x00, 0x00])
     }
 
     #[test]
     fn decode_client_registration() {
         init_logger();
         let message = Message::decode_client_registration(&[0x20, 0x00, 0x00]);
-        trace!("message: {:?}", message);
+        trace!("result: {:?}", message);
         assert!(message.is_ok());
         match message.expect("Message decoding failed") {
-            Message::Registration(ref reg) => assert_eq!(ClientType::ClientType_vehicle as ClientType_t, reg.type_),
+            Message::Registration(ref reg) => {
+                assert_eq!(ClientType::ClientType_vehicle as ClientType_t, reg.type_);
+                assert_eq!(ptr::null_mut(), reg.covered_area);
+                assert_eq!(ptr::null_mut(), reg.minimum_message_period);
+            },
+            _ => panic!("Wrong message variant")
         }
-        /*
-        if let Message::Registration(ref reg) = message {
-            assert_eq!(ClientType::ClientType_vehicle as ClientType_t, reg.type_);
-        }*/
     }
 }

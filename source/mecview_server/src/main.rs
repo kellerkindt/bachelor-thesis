6c7265d5a3e0b51b338f69bd4af36f7539d1e5d7
Michael Watzko <michael@watzko.de>
2018-04-05 16:07:38 +0200

    Add some very basic playtesting

--- source/mecview_server/src/main.rs
+++ source/mecview_server/src/main.rs
@@ -28,6 +28,8 @@ mod async {
     pub use futures::sync::mpsc::Receiver;
     pub use futures::sync::mpsc::channel;
 
+    pub use tokio::spawn;
+
     pub use command_processor::CommandProcessor;
 }
 
@@ -43,4 +45,71 @@ mod io {
         pub use tokio::net::TcpStream;
         pub use tokio::net::TcpListener;
     }
+}
+
+fn main() {
+    use std::net::SocketAddr;
+    use std::net::SocketAddrV4;
+    use async::Sink;
+    use async::Future;
+    use async::Stream;
+
+    use io::AsyncRead;
+    use io::AsyncWrite;
+    use io::net::TcpStream;
+    use io::net::TcpListener;
+
+    use libmessages::Message;
+
+    use std::thread;
+
+
+
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
+    let mut runtime = ::tokio::runtime::Runtime::new().unwrap();
+
+    let listener : TcpListener = TcpListener::bind(&"0.0.0.0:5500".parse::<SocketAddr>().unwrap()).unwrap();
+
+    runtime.spawn(listener.incoming().for_each(move |stream : TcpStream| {
+        println!("new connection");
+        let (sink, stream) = stream.framed(::adapter::asn::AsnCodec()).split();
+        ::tokio::spawn(stream.for_each(|message| {
+            println!("New message: {:?}", message);
+            Ok(())
+        }).then(|r| match r {
+            Err(_) => Err(()),
+            Ok(()) => Ok(()),
+        }));
+
+        let message : Message = Message::decode_client_registration(&[0x20, 0x00, 0x00]).unwrap();
+        sink.send(message).wait().unwrap();
+
+        println!("new connection end");
+        Ok(())
+    }).then(|r| match r {
+        Err(_) => Err(()),
+        Ok(()) => Ok(()),
+    }));
+
+    thread::sleep_ms(1_000);
+    let stream = TcpStream::connect(&"0.0.0.0:5500".parse::<SocketAddr>().unwrap()).wait().unwrap();
+    let (sink, stream) = stream.framed(::adapter::asn::AsnCodec()).split();
+    sink.send(Message::decode_client_registration(&[0x20, 0x00, 0x00]).unwrap()).wait();
+    thread::sleep_ms(10_000);
+}
+
+
+use async::Stream;
+use async::CommandProcessor;
+
+const CHANNEL_BUFFER_SIZE_CLIENT: usize = 2;
+
+fn spawn_new_client(adapter: async::Sender<adapter::Command<libmessages::Message>>) -> async::Sender<client::Command> {
+    let (sender, receiver) = async::channel(CHANNEL_BUFFER_SIZE_CLIENT);
+    let mut client = client::Client::new(adapter);
+    async::spawn(receiver.for_each(move |command : client::Command| {
+        client.process_command(command)
+    }));
+    sender
 }

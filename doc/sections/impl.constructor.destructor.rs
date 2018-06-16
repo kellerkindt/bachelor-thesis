c618d00930963ae141047a61675d5947bb26f1aa
Michael Watzko <michael@watzko.de>
2018-06-16 16:35:47 +0200

    ASN.1 impl chapter should now be ok

--- doc/sections/impl.constructor.destructor.rs
+++ doc/sections/impl.constructor.destructor.rs
@@ -5,25 +5,7 @@ extern crate libc;
 mod bindings;
 pub use bindings::*;
 
-pub unsafe fn free_content<T>(asn_type: &mut asn_TYPE_descriptor_t, value: &T) {
-    free_struct(asn_type, value, true)
-}
-
-pub unsafe fn free_struct<T>(asn_type: &mut asn_TYPE_descriptor_t, value: &T, only_content: bool) {
-    trace!("asn_free type {:?}", asn_type);
-    asn_type
-        .free_struct
-        .expect("free_struct is NULL, is the library not loaded?")(
-        asn_type as *mut asn_TYPE_descriptor_t,
-        value as *const T as *mut ::std::os::raw::c_void,
-        if only_content { 1 } else { 0 } as ::std::os::raw::c_int,
-    );
-    trace!("freed");
-}
-
-
-unsafe impl Send for ClientRegistration {}
-unsafe impl Sync for ClientRegistration {}
+use ::std::os::raw::c_int;
 
 impl Default for ClientRegistration {
     fn default() -> Self {
@@ -34,7 +16,34 @@ impl Default for ClientRegistration {
 impl Drop for ClientRegistration {
     fn drop(&mut self) {
         unsafe {
-            free_content(&mut asn_DEF_ClientRegistration, self);
+            free_content(
+                &mut asn_DEF_ClientRegistration,
+                self,
+            );
         }
     }
 }
+
+pub unsafe fn free_content<T>(
+    asn_type: &mut asn_TYPE_descriptor_t,
+    value: &T,
+) {
+    free_struct(asn_type, value, true)
+}
+
+pub unsafe fn free_struct<T>(
+    asn_type: &mut asn_TYPE_descriptor_t,
+    value: &T,
+    only_content: bool,
+) {
+    trace!("asn_free type {:?}", asn_type);
+    asn_type
+        .free_struct
+        .expect("free_struct is NULL, library not loaded?")(
+            asn_type as *mut asn_TYPE_descriptor_t,
+            value as *const T as *mut ::std::os::raw::c_void,
+            if only_content { 1 } else { 0 } as c_int,
+    );
+    trace!("freed");
+}
+
